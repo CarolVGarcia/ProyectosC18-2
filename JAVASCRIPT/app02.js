053fb8ec7757a3cb6b8a12d4654e85f50f08994b
carol Garcia <jake.v.garcia.mazatlan@gmail.com>
2024-02-02 18:14:23 -0700

    Practica 02 HTML Y JS CORREGIDOS

--- JAVASCRIPT/app02.js
+++ JAVASCRIPT/app02.js
@@ -1,62 +1,33 @@
-function hacerpeticion(){
-    const http = new XMLHttpRequest;
-    const url = "https://jsonplaceholder.typicode.com/albums";
-
-    //Validar la respuesta
-
-    http.onreadystatechange = function(){
-        if(this.status ==200 && this.readyState ==4){
-            //aqui se dibuja la pagina
-            let res = document.getElementById("lista");
-            const json = JSON.parse(this.responseText);
-
-            //CICLO PARA IR TOMANDO CADA UNO DE LOS REGISTROS
-
-            for(const datos of json){
-                res.innerHTML +='<tr> <td class="columna1">'+ datos.userId + '</td>'
-                 +'<td class="columna2">'+ datos.id + '</td>'
-                 +'<td class="columna3">'+ datos.title + '</td> </tr>'
-            }
-
-            res.innerHTML += "</tbody>"
-
-
-        }
-    }
-
-    http.open('GET',url,true);
-    http.send();
-
-}
-function buscarDatos() {
-    const searchTerm = document.getElementById("searchInput").value.toLowerCase();
+function hacerpeticion(albumId) {
     const http = new XMLHttpRequest();
-    const url = "https://jsonplaceholder.typicode.com/albums";
+    const url = `https://jsonplaceholder.typicode.com/albums/${albumId}`;
 
-    http.onreadystatechange = function () {
+    http.onreadystatechange = function() {
         if (this.status == 200 && this.readyState == 4) {
             let res = document.getElementById("lista");
-            const json = JSON.parse(this.responseText);
+            res.innerHTML = ""; 
+            const datos = JSON.parse(this.responseText);
 
-            res.innerHTML = "<tbody>"; // Limpiar tabla antes de mostrar los resultados filtrados
-
-            for (const datos of json) {
-                if (datos.title.toLowerCase().includes(searchTerm)) {
-                    res.innerHTML += '<tr> <td class="columna1">' + datos.userId + '</td>' +
-                        '<td class="columna2">' + datos.id + '</td>' +
-                        '<td class="columna3">' + datos.title + '</td> </tr>';
-                }
-            }
-
-            res.innerHTML += "</tbody>";
+            res.innerHTML += `<tr>
+                <td class="columna1">${datos.userId}</td>
+                <td class="columna2">${datos.id}</td>
+                <td class="columna3">${datos.title}</td>
+            </tr>`;
         }
-    };
+    }
 
     http.open('GET', url, true);
     http.send();
 }
-//LIMPIAR
-document.getElementById('btnLimpiar').addEventListener("click", function(){
-    let res = document.getElementById("respuesta");
-    res.innerHTML="";
+
+document.getElementById('btnBuscar').addEventListener("click", function() {
+    const albumId = document.getElementById("albumId").value;
+    if (albumId.trim() !== "") {
+        hacerpeticion(albumId);
+    }
+});
+
+document.getElementById('btnLimpiar').addEventListener("click", function() {
+    let res = document.getElementById("lista");
+    res.innerHTML = "";
 });
